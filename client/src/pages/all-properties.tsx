140a8631f4096f4d5858aea15661f88d78c1885f
Chanatinart K <chanatinart.k@gmail.com>
2023-09-22 17:48:07 +0700

    add Custom Btn set All properties page

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Add } from "@mui/icons-material";
+import { useList } from "@refinedev/core";
+import {
+  Box,
+  Typography,
+  Stack,
+  TextField,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+import { PropertyCard, CustomButton } from "../components";
 
 function AllProperties() {
-  return <div>allProperties</div>;
+  const navigate = useNavigate();
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Typography fontSize={25} fontWeight={700} color="#11142d">
+          All Properties
+        </Typography>
+        <CustomButton
+          title="Add Property"
+          handleClick={() => {
+            navigate("/properties/create");
+          }}
+          backgroundColor="#475be8"
+          color="#fcfcfc"
+          icon={<Add />}
+        />
+      </Stack>
+    </Box>
+  );
 }
 
 export default AllProperties;
