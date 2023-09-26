8c7dce7cdb870f35f2ad91ba44a033062b980e45
Chanatinart K <chanatinart.k@gmail.com>
2023-09-27 01:37:32 +0700

    complete Pagination, Sorting & Filtering

--- client/src/pages/all-properties.tsx
+++ client/src/pages/all-properties.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useMemo } from "react";
 import { Add } from "@mui/icons-material";
 import { useTable } from "@refinedev/core";
 import {
@@ -16,19 +16,42 @@ import { PropertyCard, CustomButton } from "../components";
 function AllProperties() {
   const navigate = useNavigate();
 
+  // Destructuring properties from useTable hook
   const {
-    tableQueryResult: { data, isLoading, isError },
-    current,
-    setCurrent,
-    setPageSize,
-    pageCount,
-    sorters,
-    setSorters,
-    setFilters,
-    filters,
+    tableQueryResult: { data, isLoading, isError }, // Data, loading, and error status
+    current, // Current page number
+    setCurrent, // Function to set the current page
+    setPageSize, // Function to set the page size (number of items per page)
+    pageCount, // Total number of pages
+    sorters, // Sorting configuration
+    setSorters, // Function to set sorting configuration
+    setFilters, // Function to set filters
+    filters, // Filter configuration
   } = useTable();
 
+  // Extracting properties from the data or setting an empty array if data is undefined
   const allProperties = data?.data ?? [];
+
+  // Extracting the current sorting order for the 'price' field
+  const currentPrice = sorters.find((item) => item.field === "price")?.order;
+
+  // Function to toggle the sorting order of the 'price' field
+  const toggleSort = (field: string) => {
+    setSorters([{ field, order: currentPrice === "asc" ? "desc" : "asc" }]);
+  };
+
+  // Memoizing the current filter values  that contains filter values for the "title" and "propertyType" fields.
+  const currentFilterValues = useMemo(() => {
+    const logicalFilters = filters.flatMap((item) =>
+      "field" in item ? item : []
+    );
+    return {
+      title: logicalFilters.find((item) => item.field === "title")?.value || "",
+      propertyType:
+        logicalFilters.find((item) => item.field === "propertyType")?.value ||
+        "",
+    };
+  }, [filters]);
 
   if (isLoading) return <Typography>Loading...</Typography>;
   if (isError) return <Typography>Error...</Typography>;
@@ -43,6 +66,7 @@ function AllProperties() {
               : "All Properties"}
           </Typography>
 
+          {/* Filter and Sort Controls */}
           <Box
             mb={2}
             mt={3}
@@ -57,22 +81,32 @@ function AllProperties() {
               flexWrap="wrap"
               mb={{ xs: "20px", sm: 0 }}
             >
-              {/* sorting by price */}
+              {/* sorting by Price */}
               <CustomButton
-                title={`Sort price`}
-                handleClick={() => {}}
+                title={`Sort price ${currentPrice === "asc" ? "↑" : "↓"}`}
+                handleClick={() => toggleSort("price")}
                 backgroundColor="#475be8"
                 color="#fcfcfc"
               />
-              {/* filter by title */}
+              {/* Filter by Title */}
               <TextField
                 variant="outlined"
                 color="info"
                 placeholder="Search by title"
-                value=""
-                onChange={() => {}}
+                value={currentFilterValues.title}
+                onChange={(e) => {
+                  setFilters([
+                    {
+                      field: "title",
+                      operator: "contains",
+                      value: e.currentTarget.value
+                        ? e.currentTarget.value
+                        : undefined,
+                    },
+                  ]);
+                }}
               />
-              {/* filter by type Selected */}
+              {/* Filter by Property Type */}
               <Select
                 variant="outlined"
                 color="info"
@@ -80,15 +114,42 @@ function AllProperties() {
                 required
                 inputProps={{ "aria-label": "Without label" }}
                 defaultValue=""
-                value=""
-                onChange={() => {}}
+                value={currentFilterValues.propertyType}
+                onChange={(e) => {
+                  setFilters(
+                    [
+                      {
+                        field: "propertyType",
+                        operator: "eq",
+                        value: e.target.value,
+                      },
+                    ],
+                    "replace"
+                  );
+                }}
               >
                 <MenuItem value="">All</MenuItem>
+                {[
+                  "Apartment",
+                  "Villa",
+                  "Farmhouse",
+                  "Condos",
+                  "Townhouse",
+                  "Duplex",
+                  "Studio",
+                  "Chalet",
+                ].map((type) => (
+                  <MenuItem key={type} value={type.toLowerCase()}>
+                    {type}
+                  </MenuItem>
+                ))}
               </Select>
             </Box>
           </Box>
         </Stack>
       </Box>
+
+      {/* Button to Add New Property */}
       <Stack direction="row" justifyContent="space-between" alignItems="center">
         <CustomButton
           title="Add Property"
@@ -101,7 +162,7 @@ function AllProperties() {
         />
       </Stack>
 
-      {/* Show All created Properties */}
+      {/* Display All Created Properties */}
       <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
         {allProperties.map((property) => (
           <PropertyCard
@@ -115,7 +176,7 @@ function AllProperties() {
         ))}
       </Box>
 
-      {/* Pagination */}
+      {/* Pagination Controls */}
       {allProperties.length > 0 && (
         <Box display="flex" gap={2} mt={3} flexWrap="wrap">
           {/* Previous Btn */}
@@ -126,7 +187,7 @@ function AllProperties() {
             color="#fcfcfc"
             disabled={!(current > 1)}
           />
-          {/* Show number of page */}
+          {/* Show current page number */}
           <Box
             display={{ xs: "hidden", sm: "flex" }}
             alignItems="center"
@@ -145,7 +206,8 @@ function AllProperties() {
             color="#fcfcfc"
             disabled={current === pageCount}
           />
-          {/* Selected size of page */}
+
+          {/* Dropdown to select page size */}
           <Select
             variant="outlined"
             color="info"
