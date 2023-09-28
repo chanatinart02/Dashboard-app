f5a5100c552f51541d6bf244d94a31c3b8ee18f1
Chanatinart K <chanatinart.k@gmail.com>
2023-09-29 01:52:26 +0700

    add Agents page and get all users(agents)

--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { useList } from "@refinedev/core";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { AgentCard } from "../components";
 
-function agent() {
-  return <div>agent</div>;
+function Agent() {
+  // Fetching the list of agents from users route in BE
+  const { data, isLoading, isError } = useList({ resource: "users" });
+
+  // Extracting the list of agents from the data
+  const allAgents = data?.data ?? [];
+
+  if (isLoading) return <div>loading...</div>;
+  if (isError) return <div>error...</div>;
+
+  return (
+    <Box>
+      {/* Heading */}
+      <Typography fontSize={25} fontWeight={700} color="#11142d">
+        Agents List
+      </Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          backgroundColor: "#fcfcfc",
+        }}
+      >
+        {/* Rendering each agent card */}
+        {allAgents.map((agent) => (
+          <AgentCard
+            key={agent._id}
+            id={agent._id}
+            name={agent.name}
+            email={agent.email}
+            avatar={agent.avatar}
+            noOfProperties={agent.allProperties.length}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
 }
 
-export default agent;
+export default Agent;
