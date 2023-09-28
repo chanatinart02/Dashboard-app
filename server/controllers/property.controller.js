69114ae70049e216575ca3483b67dcb621879312
Chanatinart K <chanatinart.k@gmail.com>
2023-09-28 18:02:47 +0700

    add Edit Property

--- server/controllers/property.controller.js
+++ server/controllers/property.controller.js
@@ -113,7 +113,30 @@ const createProperty = async (req, res) => {
   }
 };
 
-const updateProperty = async (req, res) => {};
+const updateProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, propertyType, location, price, photo } =
+      req.body;
+    //   Re-upload photo
+    const photoUrl = await cloudinary.uploader.upload(photo);
+
+    await Property.findByIdAndUpdate(
+      { _id: id },
+      {
+        title,
+        description,
+        propertyType,
+        location,
+        price,
+        photo: photoUrl.url || photo,
+      }
+    );
+    res.status(200).json({ message: "Property uploaded successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const deleteProperty = async (req, res) => {
   try {
